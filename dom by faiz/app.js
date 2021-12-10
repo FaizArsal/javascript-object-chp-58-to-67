b34b272fbd236e3d226649ad1d018e131d91f841
Faiz arsal <faizarsal066@gmail.com>
2021-12-10 11:25:23 +0500

    wot

---
+++ dom by faiz/app.js
@@ -0,0 +1,17 @@
+// var jimmi = document.getElementById("main")
+// console.log(jimmi.childNodes)
+// var c = document.getElementsByClassName("ren")
+// console.log(c.innerHTML)
+// var e = document.getElementById("first-name")
+// e.value="Faiz"
+// console.log(e.value)
+// var b = document.getElementById("last-name")
+// b.value="arsal"
+// console.log(b.value)
+// var ef = document.getElementById("Email")
+// ef.value="faizarsal055@gmail.com"
+// console.log(ef.value)
+// var mm = document.getElementById("form-content")
+// console.log(mm)
+// var gg = document.getElementById("last-name")
+// console.log(gg)
